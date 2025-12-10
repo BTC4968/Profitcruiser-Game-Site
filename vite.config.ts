4845eaa8817b9fd6fb94a0bf337eb61157e0c35c
BTC world <donaldo11sea@gmail.com>
2025-12-10 06:54:33 -0500

    Update proxy target to use VITE_API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
